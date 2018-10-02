115c13f62377d48d9cf00fa9310cc6e3596759b8
Vic <victordjohnson@gmail.com>
2018-10-02 15:50:34 -0500

    adjusted the bootstrapped files so that we have a clean start and none of the templated views

---
+++ build/precache-manifest.c6a3cf7bc0099105787cb3acaf012186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd509bec9087de709f2b",
+    "url": "/static/js/main.dd509bec.chunk.js"
+  },
+  {
+    "revision": "c25e2bdf45a600479593",
+    "url": "/static/js/1.c25e2bdf.chunk.js"
+  },
+  {
+    "revision": "dd509bec9087de709f2b",
+    "url": "/static/css/main.d41e5fa1.chunk.css"
+  },
+  {
+    "revision": "40ab3dfc8f14332db20847261847d906",
+    "url": "/index.html"
+  }
+];
