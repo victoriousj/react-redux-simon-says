0552835135eaf132bef7c982fe24a5e11945781b
Vic <victordjohnson@gmail.com>
2018-10-03 05:18:00 -0500

    working on the styling and layout

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <div className="container">
+          <div className="button"></div>
+          <div className="button"></div>
+          <div className="button"></div>
+          <div className="button"></div>
+          <div className="controls">
+            <div className="game-name">
+              <span>SIMON</span>
+            </div>
+            <div className="game-controls">
+              <div className="game-light"></div>
+              <div className="game-switch"></div>
+              <div className="game-reset"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
