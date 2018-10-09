3e28b4d9ef9ea28a5ecaa39594ea8a770eed3db6
Vic <victordjohnson@gmail.com>
2018-10-08 21:48:53 -0500

    i guess i got the animation for the slide to work? i do not know... it is ugly and will probably change

--- src/Components/ControlButtons.js
+++ src/Components/ControlButtons.js
@@ -1,27 +1,68 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { CSSTransition, transit } from 'react-css-transition';
 
-const ControlButtons = props => (
-    <div className="control-buttons">
-        <div className={
-            `game-light
-            ${props.isPlaying ? 'game-light-on' : ''}`
-            }></div>
-        <div 
-            onClick={() => props.startGame()}
-            className={
-                `game-switch ${props.isPlaying ? 'game-on' : 'game-off'}`}>
-            <div className="game-slider"></div>
-        </div>
-        <div 
-            onClick={() => props.changeColorScheme()}
-            className={`game-reset ${!props.isPlaying ? 'game-light-off' : ''}`}>
-        </div>
-    </div>
-)
+CSSTransition.childContextTypes = {
+    // this can be empty
+}
 
-ControlButtons.propTypes = {
-    isPlaying: PropTypes.bool.isRequired,
+const transitionStyles = {
+    defaultStyle: {
+        transform: "translateX(-1.8vh)"
+    },
+    enterStyle: {
+      transform: transit("translateX(1.8vh)", 200, "ease-in-out"),
+    },
+    leaveStyle: {
+      transform: transit("translateX(-1.8vh)", 200, "ease-in-out"),
+    },
+    activeStyle: {
+      transform: "translate(1.8vh, 0)",
+    },
+  };
+
+  const switchStyle = {
+    borderRadius: "2px",
+    background: "#bdc3c7",
+    height: "2.5vh",
+    width: "2.5vh",
+    marginTop: '.25vh'
+  };
+
+  const Switch = () => {
+      return (
+            <div style={ switchStyle }> </div>
+        )
+    }
+
+class ControlButtons extends Component {
+    static propTypes = {
+        isPlaying: PropTypes.bool.isRequired,
+    }
+
+    render() {
+        return (
+            <div className="control-buttons">
+                <div className={
+                    `game-light
+                    ${this.props.isPlaying ? 'game-light-on' : ''}`
+                    }>
+                </div>
+                <div 
+                    onClick={() => this.props.startGame()}
+                    className={`game-switch ${this.props.isPlaying ? 'game-on' : 'game-off'}`}
+                >
+                    <CSSTransition {...transitionStyles} active={this.props.isPlaying}>
+                        <Switch />
+                    </CSSTransition>
+                </div>
+                <div 
+                    onClick={() => this.props.changeColorScheme()}
+                    className={`game-reset ${!this.props.isPlaying ? 'game-light-off' : ''}`}>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default ControlButtons;
