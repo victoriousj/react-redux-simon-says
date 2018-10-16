6ad22b45d08e1c8937922741411dd83bacfb41e4
Vic Johnson <vjohnson@imagetrend.com>
2018-10-16 15:03:03 -0500

    started the process to include babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 module.exports = {
-  devtool: 'inline-sourcemap',
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js'
-  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: [
-          'react-hot',
-          'babel'
-        ]
-      }
+        exclude: [/node_modules/, /\.mp3$/],
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader'
+    },
+    {
+        test: /\.(mp3|flac)$/,
+        // include: SRC,
+        loader: 'file-loader'
+    }
     ]
   }
 };
